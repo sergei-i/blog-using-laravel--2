5e0950829ae0f3581d354794048cd700960a9631
Sergei <1161441@gmail.com>
2019-11-26 15:22:57 +0200

    Added post page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,4 +13,11 @@ class HomeController extends Controller
 
         return view('pages.index', compact('posts'));
     }
+
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return view('pages.show', compact('post'));
+    }
 }
